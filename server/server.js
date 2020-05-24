04d57ea58ed51da8c6367d0cb2c39a2bf140f192
eulalio <eulalio_cruz_nava@hotmail.com>
2020-05-24 15:07:49 -0500

    cambios en html

--- server/server.js
+++ server/server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const socketIO = require('socket.io');
-<<<<<<< HEAD
-const http  = require('http');
 
-=======
-const http = require('http');
->>>>>>> 8782647b3c01edb8ba6a0ca1b4342f1fc638c4f7
+const http  = require('http');
 const path = require('path');
 
 const app = express();
@@ -16,20 +12,12 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-<<<<<<< HEAD
 //iniciar el socket
 module.exports.io = socketIO(server);
 require('./sockets/socketServer');
-
-
-
-
-=======
 //iniciar el socket para comunicacion con el backend
 let io = socketIO(server);
 
-
->>>>>>> 8782647b3c01edb8ba6a0ca1b4342f1fc638c4f7
 server.listen(port, (err) => {
 
     if (err) throw new Error(err);
